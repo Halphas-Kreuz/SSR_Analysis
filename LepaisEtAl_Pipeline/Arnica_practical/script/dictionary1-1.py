20d39eb41d5071f0e6d5b0922e93af7aead8e6c0
yuwein31 <yuwein31@mi.fu-berlin.de>
2025-05-21 17:13:31 +0200

    large change before the matrix change, only for save

--- LepaisEtAl_Pipeline/Arnica_practical/script/dictionary1-1.py
+++ LepaisEtAl_Pipeline/Arnica_practical/script/dictionary1-1.py
@@ -14,13 +14,25 @@ with open('../new_output/AlleleInformationFile_nSSR_FullLength_ParameterSet2_sa7
     reader2 = csv.reader(file2)
     table2 = [row for row in reader2]
 
-# Print the contents of both tables
-# print("Table 1:", table1)
-print("Table 2:", table2)
-
 # Extract the 2nd and 5th elements from each row and create a dictionary
+# the dictionary need to be in both direction, let me think how to do this .
 # Assuming `table2` is already populated with the CSV data
 abbr_to_fullSeqDict = {row[1]: row[4] for row in table2 if len(row) > 4}
+reversed_abbr_to_fullSeqDict = {row[4]: row[1] for row in table2 if len(row) > 4}
 
-# Print the resulting dictionary
-print(abbr_to_fullSeqDict)
+def get_counterpart(forward, reverse, query):
+    if query in forward:
+        return forward[query]
+    elif query in reverse:
+        return reverse[query]
+    else:
+        return None
+
+#try for a key 
+word_to_search = "TGTGTGTCTATATATC(1)CATA(13)CACATGTATATATAT(1)"  # Replace with the word you want to search for
+reverseSearch = "CATGGCTGCCACACCCACTTCTTAAACCATCATCATTTTCATGTTGTATGTATGTATGTATGATTGATGATGCATGAGATA"
+
+result1= get_counterpart(abbr_to_fullSeqDict, reversed_abbr_to_fullSeqDict, word_to_search)
+result2= get_counterpart(abbr_to_fullSeqDict, reversed_abbr_to_fullSeqDict, reverseSearch)
+print("Result for word_to_search:", result1)
+print("Result for reverseSearch:", result2)
